0fbd9f2510ae29bd77a0ca66154ac28f182d0c28
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2025-07-24 16:11:17 +0000

    feat: building validation with zod and react hook form in sign up page form and making useRegisterUser hook to connect to API later

---
+++ src/http/use-register-user.tsx
@@ -0,0 +1,29 @@
+import { useMutation } from "@tanstack/react-query"
+
+type RegisterUser = {
+    name: string
+    email: string
+    password: string
+}
+
+export const useRegisterUser = () => {
+
+    return useMutation({
+        mutationFn: async (data: RegisterUser) => {
+            
+            const response = await fetch('', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    
+                },
+                body: JSON.stringify(data)
+                
+            })
+            
+            const result: RegisterUser = await response.json()
+
+            return result 
+        }
+    })
+}
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -4,7 +4,42 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Link } from "react-router-dom"
 
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { useForm } from "react-hook-form"
+import { ErrorMessage } from '@hookform/error-message'
+
+const userRegisterSchema = z.object({
+    name: z.string().nonempty('Digite um nome de usuário.'),
+    email: z.string().nonempty('Digite o seu email.'),
+    password: z.string().nonempty('Digite uma senha.'),
+    confirm_password: z.string().nonempty("Confirme a senha inserida.")
+}).refine(({ password, confirm_password}) => password === confirm_password, {
+  message: "As senhas precisam ser iguais.",
+  path: ["confirm_password"]
+})
+
+type UserRegisterType = z.infer<typeof userRegisterSchema>
+
 export const SignUp = () => {
+
+    const userRegisterForm = useForm<UserRegisterType>({
+        resolver: zodResolver(userRegisterSchema),
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    })
+
+    const handleUserRegister = (data: UserRegisterType) => {
+        console.log(data)
+
+
+        // userRegisterForm.reset()
+    }
+
+
     return (
         <>
             <div className="flex justify-center items-center h-dvh">
@@ -18,24 +53,49 @@ export const SignUp = () => {
                         </CardDescription>
                     </CardHeader>
                     <CardContent>
-                        <form className="flex flex-col gap-2">
+
+                        <form {...userRegisterForm} onSubmit={userRegisterForm.handleSubmit(handleUserRegister)} className="flex flex-col gap-2">
                             <Label>Nome:</Label>
-                            <Input placeholder="Digite seu nome"/>
+                            <Input {...userRegisterForm.register("name")} placeholder="Digite seu nome"/>
+                            <ErrorMessage
+                                errors={userRegisterForm.formState.errors}
+                                name="name"
+                                render={({ message }) => <p className="text-sm text-red-400 mt-[-20px] mb-4">{message}</p>}
+                            />
+
 
                             <Label>Email:</Label>
-                            <Input placeholder="Digite seu email"/>
+                            <Input {...userRegisterForm.register("email")} placeholder="Digite seu email"/>
+                            <ErrorMessage
+                                errors={userRegisterForm.formState.errors}
+                                name="email"
+                                render={({ message }) => <p className="text-sm text-red-400 mt-[-20px] mb-4">{message}</p>}
+                            />
 
                             <Label>Senha:</Label>
-                            <Input placeholder="Digite a sua senha"/>
+                            <Input type="password" {...userRegisterForm.register("password")} placeholder="Digite a sua senha"/>
+                            <ErrorMessage
+                                errors={userRegisterForm.formState.errors}
+                                name="password"
+                                render={({ message }) => <p className="text-sm text-red-400 mt-[-20px] mb-4">{message}</p>}
+                            />
 
                             <Label>Confirme a sua senha:</Label>
-                            <Input placeholder="••••••••••"/>
+                            <Input type="password" {...userRegisterForm.register("confirm_password")} placeholder="Confirme a sua senha"/>
+                            <ErrorMessage
+                                errors={userRegisterForm.formState.errors}
+                                name="confirm_password"
+                                render={({ message }) => <p className="text-sm text-red-400 mt-[-20px] mb-4">{message}</p>}
+                            />
+
+                            <Button type="submit" className="w-full">
+                                Cadastre-se
+                            </Button>
                         </form>
+
                     </CardContent>
                     <CardFooter className="flex flex-col">
-                        <Button className="w-full">
-                            Cadastre-se
-                        </Button>
+                        
                         <span className="text-sm mt-8 flex gap-1">
                             Já tem uma conta? 
                             
